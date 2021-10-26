23b0df95afd4cccd2250645d621b4e4ea104eb09
alekseimisner-sjsu <93103763+alekseimisner-sjsu@users.noreply.github.com>
2021-10-26 14:28:58 -0700

    page and supporting JSON file to display animals

---
+++ js/animals.js
@@ -0,0 +1,41 @@
+function getAnimals() {
+var myAnimals = [
+    {
+        "ID": "1",
+        "Name": "Bill",
+        "Type": "cat",
+        "Breed": "unknown",
+        "Gender": "male"
+    },
+    {
+        "ID": "2",
+        "Name": "Snoopy",
+        "Type": "dog",
+        "Breed": "Beagle",
+        "Gender": "male"
+    },
+    {
+        "ID": "3",
+        "Name": "Hobbes",
+        "Type": "cat",
+        "Breed": "Tiger",
+        "Gender": "male"
+    },
+    {
+        "ID": "4",
+        "Name": "Scooby Doo",
+        "Type": "dog",
+        "Breed": "Great Dane",
+        "Gender": "male"
+    },
+    {
+        "ID": "5",
+        "Name": "Mr. Ed",
+        "Type": "horse",
+        "Breed": "American Saddlebred",
+        "Gender": "male"
+    }
+]
+
+return myAnimals;
+}
