4b9c0a37467691418f4597e75e8d879e17be34e3
alekseimisner-sjsu <93103763+alekseimisner-sjsu@users.noreply.github.com>
2021-11-28 12:03:23 -0800

    adopt page overhaul
    renamed animals.html to adopt.html
    
    moved the adoptAnimals function to a separate file so it could be used on the home page too.
    
    Updated the home page to use the new adoption buttons.
    
    Updated the nav bar to say Adopt instead of Animals

---
+++ js/adopt.js
@@ -0,0 +1,37 @@
+// adoptAnimal is called when someone clicks the "Adopt NAME" button
+// in the animal search results. It gets the ID and Name of the animal
+// someone wants to adopt from the button
+// it requires a div called adoptionForm in the page that is planning
+// on using it
+function adoptAnimal(ID, Name) {
+  var animalID = arguments[0];
+  var animalName = arguments[1];
+
+  // display the adoption form. the animal id and name are passed
+  // as hidden fields, but we display the name of the anmial so
+  // people know they are trying to addopt the right animal
+  var divContainer = document.getElementById("adoptionForm");
+  divContainer.innerHTML = "";
+  divContainer.innerHTML = "<h3>Adopting " + animalName + "</h3>";
+  divContainer.innerHTML += "<img src='img/";
+  divContainer.innerHTML += "<p>Thank you for your interest in adopting " + animalName + ". The adoption fee is $100.</p>" 
+    divContainer.innerHTML += "<p>Please fill out the form below and we will get back to you regarding the next steps in the adoption process.</p><p><b>ALL FIELDS ARE REQUIRED.</b></p>";
+  divContainer.innerHTML += "<form action='./adoptAnimal.html' method='GET'>" +
+    "<input type='hidden' name='animalID' value='" + animalID + "'>" +
+    "<input type='hidden' name='animalName' value='" + animalName + "'>" +
+    "<h4>First name:</h4>" +
+    "<input type='text' id='firstname' name='firstname' required>" +
+    "<br>" +
+    "<h4>Last name:</h4>" +
+    "<input type='text' name='lastname' required>" +
+    "<br>" +
+    "<h4>Email:</h4>" +
+    "<input type='text' name='email' required>" +
+    "<br>" +
+    "<h4>Phone number:</h4>" +
+    "<input type='text' name='phonenumber' required>" +
+    "<br>" +
+    "<input type='submit' value='Submit'>" +
+    "</form>";
+  document.getElementById('firstname').focus();
+}
